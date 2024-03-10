88ca6e8d1b12cffafcd24916f10dd6042430dc40
RajdeepDs <rajdeepds626@gmail.com>
2024-03-10 22:49:09 +0530

    add oauth login (#4)

--- apps/web/src/auth.ts
+++ apps/web/src/auth.ts
@@ -1,5 +1,7 @@
 import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 import NextAuth from "next-auth";
 import prisma from "./lib/prisma";
 
@@ -9,6 +11,30 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
