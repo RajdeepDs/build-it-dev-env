94dedfe02127d7d2df7cc95d82fbcc84427b683d
RajdeepDs <rajdeepds626@gmail.com>
2024-04-06 13:31:02 +0530

    add workspace name to the dashboard sidebar

--- apps/web/src/data/workspace/get-workspace.ts
+++ apps/web/src/data/workspace/get-workspace.ts
@@ -1,10 +1,13 @@
+import { getCurrentUser } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 
-export const getWorkspace = async (userId: string) => {
+export const getWorkspace = async () => {
+  const user = await getCurrentUser();
+
   try {
     const workspace = await prisma.workspace.findFirst({
       where: {
-        adminId: userId,
+        adminId: user?.id,
       },
     });
     return workspace;
